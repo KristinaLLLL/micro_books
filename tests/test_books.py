2d2936faafe87f73877a0eeee2d7a420b85d9fdd
Kristy <k.27.06.2001.l@gmail.com>
2023-01-16 01:29:21 +0300

    Add tests for book_service

--- tests/test_books.py
+++ tests/test_books.py
@@ -6,3 +6,23 @@ def test_healthcheck():
     response = requests.get(f'{api_url}/')
     assert response.status_code == 200
 
+class BookServiceTesting():
+    def test_get_books(self):
+        response = requests.get(f'{api_url}/v1/books')
+        assert response.status_code == 200
+        assert len(response.json()) == 0
+
+    def test_add_book(self):
+        insert = {"title": "Name book", "body": "Book text"}
+        response = requests.post(f'{api_url}/v1/books', json=insert)
+        assert response.status_code == 200
+        assert response.json().get('id') == 3
+        assert response.json().get('title') == 'Name book'
+        assert response.json().get('body') == 'Book text'
+
+    def test_get_book_by_id(self):
+        response = requests.get(f'{api_url}/v1/books/3')
+        assert response.status_code == 200
+        assert response.json().get('id') == 3
+        assert response.json().get('title') == 'Name book'
+        assert response.json().get('body') == 'Book text'
