26a0fe59ab0ad6a294e54a4a684918ece4926e21
Kristy <k.27.06.2001.l@gmail.com>
2023-01-16 00:26:53 +0300

    Add test healthcheck corrected URL

--- tests/test_books.py
+++ tests/test_books.py
@@ -3,6 +3,6 @@ import requests
 api_url = 'http://localhost:8000'
 
 def test_healthcheck():
-    response = requests.get(f'{api_url}/__health')
+    response = requests.get(f'{api_url}/')
     assert response.status_code == 200
 
