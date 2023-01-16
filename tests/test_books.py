bc61f6ac236736429c28e4f776feb0bc2387410a
Kristy <k.27.06.2001.l@gmail.com>
2023-01-16 12:39:03 +0300

    efwefdf

--- tests/test_books.py
+++ tests/test_books.py
@@ -16,13 +16,13 @@ class BookServiceTesting():
         insert = {"title": "Name book", "body": "Book text"}
         response = requests.post(f'{api_url}/v1/books', json=insert)
         assert response.status_code == 200
-        assert response.json().get('id') == 3
+        assert response.json().get('id') == 4
         assert response.json().get('title') == 'Name book'
         assert response.json().get('body') == 'Book text'
 
     def test_get_book_by_id(self):
         response = requests.get(f'{api_url}/v1/books/3')
         assert response.status_code == 200
-        assert response.json().get('id') == 5
+        assert response.json().get('id') == 3
         assert response.json().get('title') == 'Name book'
         assert response.json().get('body') == 'Book text'
